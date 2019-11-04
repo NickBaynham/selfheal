db0798a9a8fe7165cb3777c10722049b829e5b3b
FLI\nbaynham <nbaynham@flhi.com>
2019-11-04 15:58:24 -0500

    created Cucumber framework project

---
+++ src/test/java/selfheal/Stepdefs.java
@@ -0,0 +1,11 @@
+package selfheal;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+
+}
