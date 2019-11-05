b9d844c50f6190e28530d9c3fc0720e13d68447d
FLI\nbaynham <nbaynham@flhi.com>
2019-11-05 13:27:47 -0500

    creating lists of elements and showing IDs

--- src/test/java/selfheal/SelfHealStepdefs.java
+++ src/test/java/selfheal/SelfHealStepdefs.java
@@ -5,9 +5,15 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
+import org.jsoup.select.NodeFilter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -39,7 +45,6 @@ public class SelfHealStepdefs {
     @When("I navigate to {string}")
     public void iNavigateTo(String url) {
         webDriver.get(url);
-        webDriver.quit();
     }
 
     @Given("I have a {string} instance")
@@ -50,5 +55,80 @@ public class SelfHealStepdefs {
     private void setupBrowser() {
         System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver_v78\\chromedriver.exe");
         webDriver = new ChromeDriver();
+    }
+
+    @Then("I close the browser")
+    public void iCloseTheBrowser() {
+        webDriver.quit();
+    }
+
+    @Then("I can print out all the element ID's")
+    public void iCanPrintOutAllTheElementIDS() {
+        Inputs inputs = new Inputs(document);
+        for (ElementObject elementObject : inputs.getElements()) {
+            System.out.print("ID: " + elementObject.getId());
+            System.out.println(", CSS Selector: " + elementObject.getCss());
+        }
+    }
+}
+
+class Inputs {
+    private Elements inputs;
+    private List<ElementObject> elements = new ArrayList<>();
+
+    Inputs(Document document) {
+        inputs = document.getElementsByTag("input");
+        for (Element input : inputs) {
+            String tag = input.tagName();
+            String id = input.id();
+            String name = input.attr("name");
+            String text = input.text();
+            String css = input.cssSelector();
+            ElementObject elementObject = new ElementObject();
+            elementObject.setTag(tag);
+            elementObject.setId(id);
+            elementObject.setCss(css);
+            elements.add(elementObject);
+        }
+    }
+
+    List<ElementObject> getElements() {
+        return elements;
+    }
+
+    Elements getMatches(String matcher) {
+        return inputs;
+    }
+}
+
+class ElementObject {
+    private String tag;
+    private String id;
+    private String name;
+    private String text;
+    private String css;
+
+    void setCss(String css) {
+        this.css = css;
+    }
+
+    String getCss() {
+        return css;
+    }
+
+    void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    void setId(String id) {
+        this.id = id;
+    }
+
+    String getId() {
+        return id;
     }
 }
--- src/test/resources/selfheal/simple.feature
+++ src/test/resources/selfheal/simple.feature
@@ -4,3 +4,5 @@ We want to navigate to a page, pull the HTML Document, and print it out on the t
     Given I have a "ChromeDriver" instance
     When I navigate to "http://localhost:1337/example1.html"
     Then I should be able to pull the HTML and print it
+    Then I close the browser
+
