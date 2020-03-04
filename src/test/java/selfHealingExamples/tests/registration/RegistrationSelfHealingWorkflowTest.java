6fb0438c0fdf52b533518629e82efbf05875dc6b
Nick Baynham <nickbaynham@gmail.com>
2020-03-04 13:41:09 -0500

    added test that uses workflow and page objects with self healing

---
+++ src/test/java/selfHealingExamples/tests/registration/RegistrationSelfHealingWorkflowTest.java
@@ -0,0 +1,22 @@
+package selfHealingExamples.tests.registration;
+
+import org.testng.annotations.Test;
+import selfHealingExamples.workflows.RegistrationSelfHealingPageObjectsWorkflow;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class RegistrationSelfHealingWorkflowTest extends BaseTest {
+
+    @Test
+    public void TestRegistrationWorkflow() throws Exception {
+        RegistrationSelfHealingPageObjectsWorkflow registrationWorkflow = new RegistrationSelfHealingPageObjectsWorkflow(getDriver());
+        assertTrue(registrationWorkflow.completeRegistration(
+                "John",
+                "Jones",
+                "JJ",
+                "Alberqurque",
+                "NM",
+                "87109",
+                false));
+    }
+}
